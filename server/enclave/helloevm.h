de339f29d1dded25cfd62512627eb5ee0ac5780d
imtypist <imtypist@163.com>
2021-12-28 12:48:43 +0800

    integrate eEVM into openenclave

---
+++ server/enclave/helloevm.h
@@ -0,0 +1,17 @@
+
+#ifndef HELLO_WORLD_SAMPLE_HELLOEVM_H
+#define HELLO_WORLD_SAMPLE_HELLOEVM_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int helloevm();
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif //HELLO_WORLD_SAMPLE_HELLOEVM_H
